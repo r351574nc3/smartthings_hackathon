8d63725197b290d89629d8a5745ea21fd15b733e
Przybylski 중광 <r351574nc3@gmail.com>
2015-03-06 21:30:39 -0700

    Adding stub for main class

---
+++ src/SmartThingsHackathon.groovy
@@ -0,0 +1,7 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
+@Grab(group='oauth.signpost', module='signpost-core', version='1.2.1.2')
+@Grab(group='oauth.signpost', module='signpost-commonshttp4', version='1.2.1.2')
+ 
+import groovyx.net.http.RESTClient
+import static groovyx.net.http.ContentType.*
+ 
