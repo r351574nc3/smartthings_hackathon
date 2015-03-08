6fb1120b915b1b33ea1016912896b3b90d91e90e
Przybylski 중광 <r351574nc3@gmail.com>
2015-03-07 20:22:27 -0700

    Stubbing out basic zendesk request stuff

--- src/SmartThingsHackathon.groovy
+++ src/SmartThingsHackathon.groovy
@@ -2,6 +2,17 @@
 @Grab(group='oauth.signpost', module='signpost-core', version='1.2.1.2')
 @Grab(group='oauth.signpost', module='signpost-commonshttp4', version='1.2.1.2')
  
-import groovyx.net.http.RESTClient
+import groovy.json.JsonSlurper
+import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.*
  
+def authHeader = 'yournetid@email.arizona.edu/token:yourtoken'.bytes.encodeBase64()
+println authHeader
+def smartthings = new HTTPBuilder("https://hackathonhelp.zendesk.com/api/v2/")
+resp = smartthings.get(path: 'views/active.json', 
+                       headers: ["Content-Type": "text/json", "Authorization": "Basic ${authHeader}"]) { resp, json ->
+
+    json.views.each() { obj ->
+        println obj.id
+    } 
+}
